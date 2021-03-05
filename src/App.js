f9b0fa35cae8c4e78c2343943a59d21249011dd6
EgriAlexandra <alexandraegri1@gmail.com>
2021-03-05 10:30:16 +0200

    fetch-app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from "react";
 import './App.css';
 
 function App() {
+  //data state// storage
+  const [ data, setData ]=useState([]);
+
+  useEffect( () => {
+    // loadData();
+    getData();
+  }, []);
+
+  // const loadData = async () => {
+  //   await fetch("https://jsonplaceholder.typicode.com/users")
+  //   .then( respponse => respponse.json())
+  //   .then( recivedData => setData(recivedData))
+  // }
+
+  //  console.log(data);
+
+  async function getData(){
+    const api = "https://jsonplaceholder.typicode.com/users";
+    const result = await fetch(api);
+    const getResult = await result.json();
+    setData(getResult);
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Fetch / Async / Await</p>
+      {data.map(record =>(
+        <div key={record.id}>{record.name}, {record.userName}</div>
+
+      ))}
     </div>
+    
   );
 }
 
